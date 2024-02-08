dfd4e0d66e3a0f3c7acf07dca8f2dcc43db54ed9
Volodymyr <volodymyrlesiv@ykr.net>
2024-02-08 12:41:51 +0200

    3 task without css

---
+++ src/components/friendlist/Friendlist.jsx
@@ -0,0 +1,25 @@
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p className={isOnline ? "online" : "offline"}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+};
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          <FriendListItem {...friend} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
