ecd2526b7108f1d9aa95b1d1445743353382026e
Volodymyr <volodymyrlesiv@ykr.net>
2024-02-12 00:41:12 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
-import TransactionHistory from './components/TransactionHistory1/TransactionHistory';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import userData from './userData.json';
 import { friends } from './friends.json';
 import { transactions } from './transactions.json';
